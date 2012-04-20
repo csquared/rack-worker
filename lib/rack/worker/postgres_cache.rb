9cdf63750651287c51378ef952f3e6368da72cd2
Chris Continanza <christopher.continanza@gmail.com>
2012-04-20 02:45:26 +0000

    small fixes

--- lib/rack/worker/postgres_cache.rb
+++ lib/rack/worker/postgres_cache.rb
@@ -2,11 +2,16 @@ module Rack
   class Worker
     class PostgresCache
       def initialize(db_url, table_name, expires_on)
-        @db = Sequel.connect db_url
+        @db = ::Sequel.connect db_url
         @table_name = table_name
         @expires_on = expires_on
         unless @db.table_exists? @table_name
-          @db << "CREATE TABLE #{@table_name} (key text, value text, expires_on timestampz)"
+          @db << "CREATE TABLE #{@table_name} (
+            id serial, 
+            key text,
+            value text, 
+            expires_on timestamptz
+          )"
         end
       end
 
